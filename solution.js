c49a48855ec8216f5f28d75a96b0ea19d39a47ed
heroine <juyen3409@gmail.com>
2025-12-15 15:32:12 +0900

    feat: setting

---
+++ solution.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+console.log(input);
