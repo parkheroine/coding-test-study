18825cdee5a0d7f0e080d7a14674888ba0c08a06
heroine <juyen3409@gmail.com>
2026-02-05 00:32:42 +0900

    feat: BOJ 10812 바구니 순서 바꾸기 splice 이용 개선

--- simulation/10812.js
+++ simulation/10812.js
@@ -14,24 +14,12 @@ function solution() {
   let arr = Array.from({ length: N + 1 }, (v, i) => i);
   for (let i = 1; i < input.length; i++) {
     const [begin, end, mid] = input[i];
-    const front = [];
-    const tail = [];
-    for (let j = 0; j <= N; j++) {
-      const cur = arr[j];
-      if (j < begin) {
-        front.push(cur);
-      } else if (begin <= j && j < mid) {
-        tail.push(cur);
-      } else if (mid <= j && j < end) {
-        front.push(cur);
-      } else if (j === end) {
-        front.push(cur);
-        front.push(...tail);
-      } else {
-        front.push(cur);
-      }
-    }
-    arr = front;
+
+    const part1 = arr.slice(mid, end + 1);
+    const part2 = arr.slice(begin, mid);
+
+    const rotated = [...part1, ...part2];
+    arr.splice(begin, rotated.length, ...rotated);
   }
 
   arr.splice(0, 1);
