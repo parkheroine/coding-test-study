876142f483ccd7713be5ef5dcbe1cf801f7c9365
heroine <juyen3409@gmail.com>
2026-02-09 00:05:29 +0900

    feat: BOJ 1744 수묶기 개선

--- simulation/1744.js
+++ simulation/1744.js
@@ -13,42 +13,43 @@ function solution() {
   const positiveNums = [];
   const negativeNums = [];
   let zeroCount = 0;
+  let oneCount = 0;
 
   for (let i = 1; i < input.length; i++) {
     const num = input[i];
-    if (num > 0) {
+    if (num === 0) {
+      zeroCount++;
+    } else if (num === 1) {
+      oneCount++;
+    } else if (num > 0) {
       positiveNums.push(num);
     } else if (num < 0) {
       negativeNums.push(num);
-    } else {
-      zeroCount++;
     }
   }
 
   positiveNums.sort((a, b) => b - a);
   negativeNums.sort((a, b) => a - b);
 
-  let result = positiveNums.length % 2 == 0 ? 0 : positiveNums.pop();
+  let result = 0;
 
-  for (let i = 0; i < positiveNums.length - 1; i += 2) {
-    result += Math.max(
-      positiveNums[i] + positiveNums[i + 1], //둘 중 하나가 1일 때
-      positiveNums[i] * positiveNums[i + 1],
-    );
-  }
-
-  if (negativeNums.length % 2 != 0) {
-    if (zeroCount != 0) {
-      negativeNums.pop();
+  for (let i = 0; i < positiveNums.length; i += 2) {
+    if (i + 1 < positiveNums.length) {
+      result += positiveNums[i] * positiveNums[i + 1];
     } else {
-      result += negativeNums.pop();
+      result += positiveNums[i];
     }
   }
 
-  for (let i = 0; i < negativeNums.length - 1; i += 2) {
-    result += negativeNums[i] * negativeNums[i + 1];
+  for (let i = 0; i < negativeNums.length; i += 2) {
+    if (i + 1 < negativeNums.length) {
+      result += negativeNums[i] * negativeNums[i + 1];
+    } else {
+      result += zeroCount > 0 ? 0 : negativeNums[i];
+    }
   }
 
+  result += oneCount;
   return result;
 }
 
